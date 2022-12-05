52e1dc8a9bb7d11bbc2fcddaee815173b02d354b
arthurmlv <arthurmv123@gmail.com>
2022-12-05 10:40:25 -0300

    Modelo funcionando
    Pode ser testado e visualizado na pasta notebooks.

--- src/features/logistic-reg-extraction.py
+++ src/features/logistic-reg-extraction.py
@@ -1,6 +1,5 @@
 from sklearn import datasets
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.datasets import load_files
 import pickle
 import numpy as np
 import pandas as pd
--- src/models/logistic-regression-model.py
+++ src/models/logistic-regression-model.py
@@ -1,26 +1,43 @@
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
-
 ## Reading files:
 
-dataset = pd.read_pickle("data/processed/database.pkl")
-dataY = pd.read_pickle("data/processed/dataY.pkl")
+dataset = pd.read_pickle("C:/Users/arthu/Desktop/MDS/2022-2-Squad03/data/processed/database.pkl")
+dataY = pd.read_pickle("C:/Users/arthu/Desktop/MDS/2022-2-Squad03/data/processed/dataY.pkl")
 
 ## Splitting into training and testing data:
+reshaped_dataset = dataset
 
-X_train, X_test, Y_train, Y_test = train_test_split(dataset, dataY, test_size=0.3, stratify=dataY, random_state=777)
+bigram_vectorizer = CountVectorizer(ngram_range=(2, 2),
+                            lowercase=True,
+                            analyzer='char',
+                            strip_accents="unicode")
+bigram_vectorizer.fit(reshaped_dataset)
+X = bigram_vectorizer.transform(reshaped_dataset)
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, dataY, test_size=0.3, stratify=dataY, random_state=12)
 
 ## Training the model:
 
-model = LogisticRegression()
+model = BernoulliNB(binarize = True)
 model.fit(X_train, Y_train)
+print(model)
 
 ##  Accuracy score:
 
 from sklearn.metrics import accuracy_score
 
-X_train_prediction = model.predict(X_train)
-training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
+Y_pred = model.predict(X_test)
 
-print('Acc:', training_data_accuracy)
+acc = accuracy_score(Y_test, Y_pred) ##0.97
+print(acc)
+
+## Compressing Model
+
+import pickle
+
+pickle.dump(model, open("C:/Users/arthu/Desktop/MDS/2022-2-Squad03/models/bernoulli.sav", 'wb'))
+
