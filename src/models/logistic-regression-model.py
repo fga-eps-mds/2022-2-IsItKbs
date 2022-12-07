a20c604aa6ed4ae7c5b1f6fa5f2986f247066cf3
G16C <gabrielcampello2002@gmail.com>
2022-12-06 21:13:55 -0300

    https://github.com/fga-eps-mds/2022-2-Squad03/tree/devel

--- src/models/logistic-regression-model.py
+++ src/models/logistic-regression-model.py
@@ -4,8 +4,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 ## Reading files:
 
-dataset = pd.read_pickle("C:/Users/arthu/Desktop/MDS/2022-2-Squad03/data/processed/database.pkl")
-dataY = pd.read_pickle("C:/Users/arthu/Desktop/MDS/2022-2-Squad03/data/processed/dataY.pkl")
+dataset = pd.read_pickle("data/processed/database.pkl")
+dataY = pd.read_pickle("data/processed/dataY.pkl")
 
 ## Splitting into training and testing data:
 reshaped_dataset = dataset
@@ -33,11 +33,31 @@ from sklearn.metrics import accuracy_score
 Y_pred = model.predict(X_test)
 
 acc = accuracy_score(Y_test, Y_pred) ##0.97
-print(acc)
+print('Acc:', acc)
+
+## Evaluating the model with new metrics
+
+from sklearn.metrics import balanced_accuracy_score
+
+score1 = balanced_accuracy_score(Y_test, Y_pred)
+
+print('Acc:', score1)
+
+from sklearn.metrics import f1_score
+
+score2 = f1_score(Y_test, Y_pred, labels=None, pos_label='0', average='binary', sample_weight=None, zero_division='warn')
+
+print('Acc:', score2)
+
+from sklearn.metrics import recall_score
+
+score3 = recall_score(Y_test, Y_pred, labels=None, pos_label='0', average='binary', sample_weight=None, zero_division='warn')
+
+print('Acc:', score3)
 
 ## Compressing Model
 
 import pickle
 
-pickle.dump(model, open("C:/Users/arthu/Desktop/MDS/2022-2-Squad03/models/bernoulli.sav", 'wb'))
+pickle.dump(model, open("models/bernoulli.sav", 'wb'))
 
